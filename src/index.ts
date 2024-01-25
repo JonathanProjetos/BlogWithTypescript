5123a5531f27489866572af4cfbc909ae16618c9
Jonathan Santos <jonathan01347@gmail.com>
2024-01-25 16:39:55 -0300

    feat(index)Add index.ts file with App class and server start method

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import express from 'express'
+import 'dotenv/config'
+import 'express-async-errors'
+import { Connection } from './config/connection'
+
+export class App {
+  app: express.Express
+  readonly PORT: string | undefined = process.env.PORT
+  readonly connection: Connection
+
+  constructor () {
+    this.app = express()
+
+    this.connection = new Connection()
+
+    this.app.get('/', (_req: express.Request, res: express.Response) => {
+      res.status(200).json({ message: 'está funcionando' })
+    })
+
+    this.handleError()
+  }
+
+  private handleError (): void {
+    this.app.use((
+      err: Error,
+      _req: express.Request,
+      res: express.Response,
+      _next: express.NextFunction
+    ) => {
+      const [status, message] = err.message.split('|')
+      if (err.message.split('').includes('|')) {
+        res.status(Number(status)).json({ error: message })
+      } else {
+        console.error('Error não Mapeado: ', err)
+        console.error('Error não Mapeado: ', err.message)
+        res.status(500).json({ error: err.message })
+      }
+    })
+  }
+
+  public start (): void {
+    try {
+      this.connection.connect()
+      this.app.listen(this.PORT, () => {
+        console.log(`Servidor rodando na porta ${this.PORT}`)
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
+
+const server = new App()
+server.start()
