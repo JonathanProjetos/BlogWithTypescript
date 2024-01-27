84bf448f54a88ffd94bcf7e2cc3c874af955ac39
Jonathan Santos <jonathan01347@gmail.com>
2024-01-27 15:34:30 -0300

    fix(userODM)Refactor UserODM class to improve code readability and maintainability

--- src/models/user/userODM.ts
+++ src/models/user/userODM.ts
@@ -21,13 +21,29 @@ class UserODM {
         type: String,
         required: true
       }
+    }, {
+      versionKey: false
     })
 
     this.model = model<IUser>('user', this.schema)
   }
 
   public async create (user: IUser): Promise<IUser> {
-    return await this.model.create({ ...user })
+    const { email, password, displayName } = user
+
+    return await this.model.create({
+      email,
+      password,
+      displayName
+    })
+  }
+
+  public async getUserByEmail (email: string): Promise <IUser | null> {
+    return await this.model.findOne({ email })
+  }
+
+  public async getUserById (id: string): Promise <IUser | null> {
+    return await this.model.findOne({ _id: id })
   }
 }
 
