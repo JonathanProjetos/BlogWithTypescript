b484a4af1e618ac70b7097e280b4c7e9b6d23703
Jonathan Santos <jonathan01347@gmail.com>
2024-01-27 15:38:32 -0300

    feat(index)Add user router and middleware

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,24 @@ import express from 'express'
 import 'dotenv/config'
 import 'express-async-errors'
 import { Connection } from './config/connection'
+import userRouter from './router/user/userRouter'
 
-export class App {
+class App {
   app: express.Express
   readonly PORT: string | undefined = process.env.PORT
   readonly connection: Connection
 
   constructor () {
     this.app = express()
+    this.app.use(express.json())
 
     this.connection = new Connection()
 
     this.app.get('/', (_req: express.Request, res: express.Response) => {
       res.status(200).json({ message: 'está funcionando' })
     })
+
+    this.app.use('/', userRouter)
 
     this.handleError()
   }
@@ -40,7 +44,6 @@ export class App {
 
   public start (): void {
     try {
-      this.connection.connect()
       this.app.listen(this.PORT, () => {
         console.log(`Servidor rodando na porta ${this.PORT}`)
       })
