fb1c7ebfc9fe783538804ed0ea6892b2aef5cf6a
Jonathan Santos <jonathan01347@gmail.com>
2024-01-29 16:06:55 -0300

    feat(authService)Add AuthService with login functionality

---
+++ src/services/auth/authService.ts
@@ -0,0 +1,27 @@
+import type UserODM from '../../models/user/userODM'
+import { type IAuthInput } from '../../interfaces/IAuth'
+import { validateInputAuth } from '../../middlewares/joiInputAuth'
+import bcryptJs from 'bcryptjs'
+import { generateToken } from '../../middlewares/token'
+
+class AuthService {
+  constructor (private readonly userODM: typeof UserODM) {}
+
+  login = async (body: IAuthInput): Promise<string | null> => {
+    const { email, password } = validateInputAuth(body)
+
+    const isUser = await this.userODM.getUserByEmail(email)
+
+    if (isUser === null) throw new Error('404|User not found')
+
+    if (!bcryptJs.compareSync(password, isUser.password)) {
+      throw new Error('401|Incorrect password')
+    }
+
+    const token = generateToken(email)
+
+    return token
+  }
+}
+
+export default AuthService
