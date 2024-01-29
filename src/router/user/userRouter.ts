a9e4b0625820d0991dd01e7b4443ee5040718daa
Jonathan Santos <jonathan01347@gmail.com>
2024-01-29 16:11:02 -0300

    feat(userRouter)Update userRouter.ts with new imports and routes

--- src/router/user/userRouter.ts
+++ src/router/user/userRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import UserODM from '../../models/user/userODM'
 import UserService from '../../services/user/userService'
-import UserController from '../../controllers/userController'
+import UserController from '../../controllers/user/userController'
+import { validateToken } from '../../middlewares/decodedToken'
 
-const model = new UserODM()
+const model = UserODM
 const service = new UserService(model)
 const controller = new UserController(service)
 
@@ -11,5 +12,7 @@ const userRouter = Router()
 
 userRouter.post('/users', controller.create)
 userRouter.get('/users/:id', controller.getUserById)
+userRouter.delete('/users/:id', validateToken, controller.getUserAndDelete)
+userRouter.put('/users/:id', validateToken, controller.getUserAndUpdate)
 
 export default userRouter
