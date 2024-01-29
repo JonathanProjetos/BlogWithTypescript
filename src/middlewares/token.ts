f26a04a7c35c64b740fd75830cc9ab90b13a9221
Jonathan Santos <jonathan01347@gmail.com>
2024-01-29 16:22:30 -0300

    refactor(token)Refactor code and add authentication routes

--- src/middlewares/token.ts
+++ src/middlewares/token.ts
@@ -1,26 +1,30 @@
 import 'dotenv/config'
-import jwt from 'jsonwebtoken'
+import { verify, sign, type JwtPayload } from 'jsonwebtoken'
 
 const secret = process.env.JWT_SECRET
 
-const generateToken = (payload: string): string => {
+const generateToken = (email: string): string => {
   if (secret === null || secret === undefined) {
     throw new Error('404|payload is not difined')
   }
-  const token = jwt.sign({ payload }, secret, {
+  const token = sign({ email }, secret, {
     expiresIn: '1d',
     algorithm: 'HS256'
   })
   return token
 }
 
-const verifyToken = (token: string): jwt.JwtPayload => {
+const verifyToken = (token: string | undefined): JwtPayload => {
   if (token === null || secret === undefined) {
     throw new Error('404|payload is not difined')
   }
 
+  if (typeof token === 'undefined') {
+    throw new Error('401|Token must be provided')
+  }
+
   try {
-    const payload = jwt.verify(token, secret) as jwt.JwtPayload
+    const payload = verify(token, secret) as JwtPayload
     return payload
   } catch (err) {
     throw new Error('401|Token must be a valid token')
