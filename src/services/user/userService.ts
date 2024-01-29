8c0d34fce94c7e7285d342a421b628f1870265da
Jonathan Santos <jonathan01347@gmail.com>
2024-01-29 16:09:49 -0300

    feat(userService)Refactor UserService class and update create, getUserAndDelete, and getUserAndUpdate methods

--- src/services/user/userService.ts
+++ src/services/user/userService.ts
@@ -1,23 +1,27 @@
-import type IUser from '../../interfaces/IUser'
+import type { IUser } from '../../interfaces/IUser'
 import type UserODM from '../../models/user/userODM'
 import { validateBodyUser } from '../../middlewares/joiBodyUser'
 import encripty from '../../helper/encrypt'
 
 class UserService {
-  constructor (private readonly userODM: UserODM) {}
+  constructor (private readonly userODM: typeof UserODM) {}
 
   create = async (body: IUser): Promise<IUser> => {
-    const checkBody = validateBodyUser(body)
+    let { email, password, displayName } = validateBodyUser(body)
 
     const isUser = await this.userODM.getUserByEmail(body.email)
 
     if (isUser !== null) throw new Error('409|user already registered')
 
-    const password = encripty(checkBody.password)
+    const encryptPassword = encripty(password)
 
-    checkBody.password = password
+    password = encryptPassword
 
-    const user = await this.userODM.create(checkBody)
+    const user = await this.userODM.create({
+      email,
+      password,
+      displayName
+    })
     return user
   }
 
@@ -27,6 +31,44 @@ class UserService {
     if (isUser === undefined) throw new Error('404|User not found')
 
     return isUser
+  }
+
+  getUserAndDelete = async (id: string, userEmail: string): Promise<IUser | null> => {
+    if (userEmail === undefined) throw new Error('401|Unauthorized')
+
+    const isUser = await this.userODM.getUserByEmail(userEmail)
+
+    if (isUser === null) throw new Error('404|User not found')
+
+    if (isUser.id !== id) throw new Error('401|Unauthorized')
+
+    const resolve = await this.userODM.deleteUser(id)
+
+    return resolve
+  }
+
+  getUserAndUpdate = async (id: string, userEmail: string, body: IUser): Promise<IUser | null> => {
+    let { email, password, displayName } = validateBodyUser(body)
+
+    if (userEmail === undefined) throw new Error('401|Unauthorized')
+
+    const isUser = await this.userODM.getUserByEmail(userEmail)
+
+    if (isUser === null) throw new Error('404|User not found')
+
+    const allUsers = await this.userODM.getAllUsers()
+
+    const isEmail = Array.isArray(allUsers) && allUsers.some((user: IUser) => user.email === email)
+
+    if (isEmail) throw new Error('409|Email already registered')
+
+    const encryptPassword = encripty(password)
+
+    password = encryptPassword
+
+    const updateUser = await this.userODM.updateUser({ email, password, displayName }, id)
+
+    return updateUser
   }
 }
 
