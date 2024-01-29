8fc91cd7f28bcdb17db23d1c3847d9b8b7f6f44e
Jonathan Santos <jonathan01347@gmail.com>
2024-01-29 16:07:45 -0300

    feat(authController)Add AuthController for handling user authentication

---
+++ src/controllers/auth/authController.ts
@@ -0,0 +1,14 @@
+import { type Request, type Response } from 'express'
+import type AuthService from '../../services/auth/authService'
+
+class AuthController {
+  constructor (private readonly authService: AuthService) {}
+
+  login = async (req: Request, res: Response): Promise<Response> => {
+    const { email, password } = req.body
+    const token = await this.authService.login({ email, password })
+    return res.status(200).json({ token })
+  }
+}
+
+export default AuthController
