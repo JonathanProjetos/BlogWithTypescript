e8bf66765e336a113f8668a2d4954f2a65459858
Jonathan Santos <jonathan01347@gmail.com>
2024-02-01 17:01:04 -0300

    Refactor userController methods

--- src/controllers/user/userController.ts
+++ src/controllers/user/userController.ts
@@ -5,15 +5,15 @@ import type { IUserController } from '../../interfaces/IUser'
 class UserController implements IUserController {
   constructor (private readonly userService: UserService) {}
 
-  public async createUser (req: Request, res: Response): Promise<Response> {
+  public createUser = async (req: Request, res: Response): Promise<Response> => {
     const { email, password, displayName } = req.body
 
-    const user = await this.userService.create({ email, password, displayName })
+    const user = await this.userService.createUser({ email, password, displayName })
 
     return res.status(201).json(user)
   }
 
-  public async getUserById (req: Request, res: Response): Promise<Response> {
+  public getUserById = async (req: Request, res: Response): Promise<Response> => {
     const { id } = req.params
 
     const userById = await this.userService.getUserById(id)
@@ -21,7 +21,7 @@ class UserController implements IUserController {
     return res.status(200).json(userById)
   }
 
-  public async getUserAndDelete (req: Request, res: Response): Promise<Response> {
+  public getUserAndDelete = async (req: Request, res: Response): Promise<Response> => {
     const { id } = req.params
     const { userEmail } = req.body
 
@@ -30,7 +30,7 @@ class UserController implements IUserController {
     return res.status(200).json(result)
   }
 
-  public async getUserAndUpdate (req: Request, res: Response): Promise<Response> {
+  public getUserAndUpdate = async (req: Request, res: Response): Promise<Response> => {
     const { id } = req.params
     const { email, password, displayName, userEmail } = req.body
 
