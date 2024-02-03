7118f634a47b4601a04437a96fa94c66e4a6d738
Jonathan Santos <jonathan01347@gmail.com>
2024-02-02 23:10:51 -0300

    Add errorLauncher middleware

---
+++ src/middlewares/errorLauncher.ts
@@ -0,0 +1,14 @@
+import type { Request, Response, NextFunction } from 'express'
+
+const errorLauncher = ( err: Error, _req: Request, res: Response,_next: NextFunction) => {
+  const [status, message] = err.message.split('|')
+  if (err.message.split('').includes('|')) {
+    res.status(Number(status)).json({ error: message })
+  } else {
+    console.error('Error não Mapeado: ', err)
+    console.error('Error não Mapeado: ', err.message)
+    res.status(500).json({ error: err.message })
+  }
+}
+
+export default errorLauncher
