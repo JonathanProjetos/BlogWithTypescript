a709537814a5d0aa008bd042aa4bd028ec79589c
Jonathan Santos <jonathan01347@gmail.com>
2024-02-02 23:20:26 -0300

    Update validation error messages in joiBodyPost.ts

--- src/middlewares/joiBodyPost.ts
+++ src/middlewares/joiBodyPost.ts
@@ -4,14 +4,14 @@ import { type IPostIput } from '../interfaces/IPost'
 const validateBodyPost = (body: IPostIput): IPostIput => {
   const postSchema = joi.object({
     title: joi.string().required().messages({
-      'any.required': '404|The "title" field is mandatory',
-      'string.empty': '404|The "title" field is not allowed to be empty',
-      'string.base': '404| The "title" field cannot be of type number'
+      'any.required': '400|The "title" field is mandatory',
+      'string.empty': '400|The "title" field is not allowed to be empty',
+      'string.base': '400| The "title" field cannot be of type number'
     }),
     content: joi.string().required().messages({
-      'any.required': '404|The "content" field is mandatory',
-      'string.empty': '404|The "content" field is not allowed to be empty',
-      'string.base': '404|The "content" field cannot be of type number'
+      'any.required': '400|The "content" field is mandatory',
+      'string.empty': '400|The "content" field is not allowed to be empty',
+      'string.base': '400|The "content" field cannot be of type number'
     })
   })
 
